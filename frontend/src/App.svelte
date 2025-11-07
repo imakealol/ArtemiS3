163aca2aaab0de55166120bfe12d3b9092a2be45
Samuel Bodenhemier <swb79@nau.edu>
2025-11-07 16:28:47 -0700

    Search page prototype progress

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import {Search as SearchIcon} from "@lucide/svelte"
+
   let name = "ArtemiS3";
   async function ping() {
     const res = await fetch("/api/test?name=Svelte");
@@ -8,9 +10,28 @@
 </script>
 
 <main>
-  <h1 class="text-red-500">Welcome to {name}</h1>
-  <p>Frontend is up. NGINX proxies this page and /api to FastAPI.</p>
-  <button on:click={ping}>Test API</button>
+  <div class="w-screen h-screen p-2">
+    <h1 class="text-red-500">Welcome to {name}</h1>
+    <p>Frontend is up. NGINX proxies this page and /api to FastAPI.</p>
+    <button on:click={ping} class="button">Test API</button>
+    <h2>Search Page Prototype:</h2>
+      <div class="aspect-16/9 mt-2 w-6xl border-2">
+        <div class="border-b h-1/6 flex flex-col items-center justify-center">
+          <h1>ArtemiS3</h1>
+        </div>
+        <div class="flex flex-col h-5/6 items-center justify-evenly">
+          <div class="flex flex-col items-center">
+            <div class="text-nowrap w-80 flex flex-col">
+              <input class="w-full shadow" placeholder="Search for Files" />
+              <SearchIcon class="relative left-[calc(100%-28px)] bottom-[calc(100%-28px)] text-gray-500" />
+            </div>
+          </div>
+          <div class="w-4/5 text-center border h-3/5">
+            results
+          </div>
+        </div>
+      </div>
+  </div>
 </main>
 
 <style>
